4b5198e241e6c115b8bd7f94275d5864ea26239b
olafursj23 <olafursj23@ru.is>
2026-09-24 11:49:27 +0000

    PI_initial

---
+++ Part_1/src/PI_controller.cpp
@@ -0,0 +1,9 @@
+#include "PI_controller.h"
+
+PI_controller::PI_controller(double parm_arg) {
+    double parm = parm_arg
+}
+
+double PI_controller::update(double ref, double actual) {
+    return
+}
