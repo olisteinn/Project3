44efbc9ba796e7c9373395ae1e36f5c841230862
olafursj23 <olafursj23@ru.is>
2026-09-26 17:30:03 +0000

    stopped 2hz blink

--- Part_1/src/StopState.cpp
+++ Part_1/src/StopState.cpp
@@ -5,20 +5,26 @@
 #include "drive.h"
 #include "digital_out.h"      
 #include "uart.h"
-
+#include "timer.h"
+#include <stdint.h>
 
 extern Drive bridge;
 extern Digital_out led;
+uint32_t led_ms;
 
 void StopState::on_do()
 {
-    //led toggle
+    if ((time_ms()-led_ms) >= 250) {
+        led_ms = time_ms();
+        led.toggle();
+    }
 }
 
 void StopState::on_entry()
 {
     bridge.stop();
     serial_println("Stopped");
+    led_ms = time_ms();
 }
 
 void StopState::on_exit()
